8f02311ebb62d2d06e2a81200d1082849f159fe5
Edwin Narvaez <davidnarvaezmiranda97@gmail.com>
2022-10-29 23:23:17 -0500

    The section 4 is done.

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,44 @@
+import { CarsService } from './cars.service';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+    constructor(
+        private readonly carsService: CarsService
+    ){}
+    
+    
+    @Get()
+    getAllCars() {
+        return this.carsService.findAll();
+    }
+
+    @Get(':id')
+    getCarById(@Param('id', ParseIntPipe) id:number){
+        return this.carsService.findOneById(id);
+    }
+
+    @Post()
+    createCar(@Body() body:any){
+        return body;
+        
+    }
+
+    @Patch(':id')
+    updateCar(
+        @Body() body:any,
+        @Param('id', ParseIntPipe) id:number )
+    {
+        return body;
+        
+    }
+
+    @Delete(':id')
+    deleteCar(@Param('id', ParseIntPipe) id:number){
+        return {
+            method: 'Delete',
+            id
+        };
+        
+    }
+}
